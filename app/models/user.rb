2d2ccbc92e891080f77c3496ed83d21e6eba8f59
andy zhen <andyzhen44@gmail.com>
2022-05-10 01:01:12 -0700

    rspec unit testing implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
+  before_save do
+    self.email.downcase! if self.email
+  end
   has_secure_password
-  validates :name, presence: true
-  validates :email, presence: true
-  # validates :password_digest, presence: true, length: {minimum:5}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: {minimum:5}
+  
+
+  def self.authenticate_with_credentials(email, password)
+    email.strip!
+    email.downcase!
+    @user = User.find_by_email(email)
+    if @user && @user.authenticate(password)
+      return @user.authenticate(password)
+    else
+      return nil
+    end
+  end
+
 end
